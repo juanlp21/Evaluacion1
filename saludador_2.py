f1e0e1583f62471a28b897d6a4e35f7ea315e300
juanlp21 <55576203+juanlp21@users.noreply.github.com>
2019-09-26 09:32:22 +0200

    Add files via upload

---
+++ saludador_2.py
@@ -0,0 +1,5 @@
+def saludador_2():
+    print "Hola amigo" #Instruccion de salida
+    name= raw_input("¿Como te llamas?: ") #Instruccion de salida
+    print "Bienvenido a Python " + name
+saludador_2 ()
