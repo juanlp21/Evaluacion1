7ec02cbf7a2af52d0071d7ec1c1366e10fb527a6
juanlp21 <55576203+juanlp21@users.noreply.github.com>
2019-10-08 11:51:12 +0200

    Add files via upload

---
+++ contador_nums_enteros.py
@@ -0,0 +1,21 @@
+#Programa para detectar numeros enteros
+def ejemplo_for():
+    #x es una variable que toma los valores comprendidos en el rango
+    print "Contador"
+    final=input("¿Hasta que numero quieres que cuente?: ")
+    for x in range (1, final+1):
+        print "x= ",x
+    if (final<0):
+        print "Imposibol"
+
+
+ejemplo_for()
+    
+
+        
+    
+    
+    
+     
+    
+    
