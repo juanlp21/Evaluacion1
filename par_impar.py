68aa9c7e107ea1c9a4e76e4534e363f2dcda9632
juanlp21 <55576203+juanlp21@users.noreply.github.com>
2019-10-15 11:16:38 +0200

    Add files via upload

---
+++ par_impar.py
@@ -0,0 +1,9 @@
+#Detector de pares e impares
+def par_impar():
+    numero=int(input ("Escriba un numero "))
+    if numero %2:
+        print "Es impar"
+    else:
+        print "Es par"
+
+par_impar()
